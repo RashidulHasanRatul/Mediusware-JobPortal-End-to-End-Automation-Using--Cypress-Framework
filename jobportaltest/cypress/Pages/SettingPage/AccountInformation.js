126bfe1227690902b60f55cca32a0581a9400ab6
RashidulHasanRatul <rashidul35-278@diu.edu.bd>
2021-09-23 20:15:44 +0600

    Acinformation Page Update

--- jobportaltest/cypress/Pages/SettingPage/AccountInformation.js
+++ jobportaltest/cypress/Pages/SettingPage/AccountInformation.js
@@ -1,14 +1,36 @@
+export default class AccountInformation {
 
-export  default  class AccountInformation {
 
-
-    hey(){
+    hey() {
         cy.log("Test");
     }
 
-    clickOnSettingButton(){
-       return  cy.get('[href="/user/settings"] > .dashboard-icon');
+    clickOnSettingButton() {
+        return cy.get('[href="/user/settings"] > .dashboard-icon');
     }
+
+    profilePicture() {
+        return cy.get('#imgInp');
+    }
+
+    cvUpload() {
+        return cy.get('#resume');
+    }
+
+    confirmPassword(){
+        return cy.get(':nth-child(5) > .form-control');
+    }
+
+
+    saveButton(){
+       return  cy.get('.save-btn');
+    }
+
+    succesfullySaveMsg(){
+        return cy.get('.mt-4')
+    }
+
+
 
 
 }
--- jobportaltest/cypress/integration/Testcases/SettingTest/TC01_AccountInformationSettingtest.js
+++ jobportaltest/cypress/integration/Testcases/SettingTest/TC01_AccountInformationSettingtest.js
@@ -1,3 +1,4 @@
+import 'cypress-file-upload';
 import AccountInformation from '../../../Pages/SettingPage/AccountInformation.js';
 import Loginpage from "../../../Pages/LoginPage/Login";
 
@@ -21,6 +22,40 @@ describe("Account Information", () => {
 
     it('should Click Setting Button', function () {
         acinfo.clickOnSettingButton().click();
+    });
+
+
+    it('Change Profile Picture', function () {
+        const imgpath = 'Asset/Birds.jpeg';
+        acinfo.profilePicture().attachFile(imgpath);
+
+
+    });
+
+    it('Change Profile Picture', function () {
+        const imgpath = 'Asset/Birds.jpeg';
+        acinfo.profilePicture().attachFile(imgpath);
+
+
+    });
+
+    it('Cv Upload', function () {
+        const cv = 'Asset/cv.pdf';
+        acinfo.cvUpload().attachFile(cv);
+    });
+
+    it('Confirm Password', function () {
+        acinfo.confirmPassword().type("123456");
+
+    });
+
+    it('Click on Save Button', function () {
+       acinfo.saveButton().click();
+
+    });
+
+    it('After save Account information Confirmation should be Show', function () {
+        acinfo.succesfullySaveMsg().should('contain.text','Your profile has been saved successfully');
     });
 
 
