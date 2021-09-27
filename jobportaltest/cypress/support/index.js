a0acea8e36e9848291a1a89946428fd1acf2e6ee
RashidulHasanRatul <rashidul35-278@diu.edu.bd>
2021-09-27 19:34:45 +0600

    Mocha Awesome Reports Implement

--- jobportaltest/cypress/support/index.js
+++ jobportaltest/cypress/support/index.js
@@ -18,3 +18,11 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+import addContext from "mochawesome/addContext";
+
+Cypress.on("test:after:run", (test, runnable) => {
+    if (test.state === "failed") {
+        const screenshot = `assets/${Cypress.spec.name}/${runnable.parent.title} -- ${test.title} (failed).png`;
+        addContext({ test }, screenshot);
+    }
+});
