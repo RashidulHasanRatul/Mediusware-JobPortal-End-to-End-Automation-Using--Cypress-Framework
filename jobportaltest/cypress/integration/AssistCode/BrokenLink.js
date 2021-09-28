24534580e937c18f1f7143aa7de41ba0a3c44492
RashidulHasanRatul <rashidul35-278@diu.edu.bd>
2021-09-28 16:16:50 +0600

    Broken Link

---
+++ jobportaltest/cypress/integration/AssistCode/BrokenLink.js
@@ -0,0 +1,27 @@
+import config from '../../../cypress.json'
+
+describe('Test Broken Links', () => {
+
+    it('verify navigation accross the pages', () =>{
+
+        cy.visit(`${config.URL1}`)
+
+        // for handle uncaught exception
+        cy.on('window:confirm', cy.stub().as('confirm'))
+        Cypress.on('uncaught:exception', (err, runnable) => {
+            // returning false here prevents Cypress from
+            // failing the test
+            return false
+        })
+
+        cy.wrap('passed').as('ctrl')
+        cy.get("a:not([href*='mailto:]']").each($el => {
+
+            if ($el.prop('href').length > 0) {
+                const message = $el.text()
+                expect($el, message).to.have.attr("href").not.contain("undefined")
+                cy.log($el.attr('href'))
+            }
+        })
+    })
+})
